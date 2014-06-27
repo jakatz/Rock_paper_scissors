045cecee2bc5db29161a02d66847e8875dd264f7
Gideon Wulfsohn <gideon.wulfsohn@gmail.com>
2014-06-26 19:44:13 -0700

    finding if game is over and update winner in games table, winner in round, and win count in player table

--- spec/orm_spec.rb
+++ spec/orm_spec.rb
@@ -9,7 +9,11 @@ describe 'ORM' do
     RPS.orm.create_tables
   end
 
-  before(:each) do
+  before(:all) do
+    RPS.orm.create_tables
+  end
+
+  after(:each) do
     RPS.orm.drop_tables
     RPS.orm.create_tables
   end
@@ -55,7 +59,7 @@ describe 'ORM' do
           p1 = RPS.orm.add_player('Gideon', '1234')
           p2 = RPS.orm.add_player('Jon', '321')
           game = RPS.orm.add_game(p1.id, p2.id, -1)
-          RPS.orm.mark_winner(game, p1.id)
+          RPS.orm.mark_winner(game.id, p1.id)
           game = RPS.orm.select_game( game.id )
           p1 = RPS.orm.select_player( p1.username )
           expect(game.winner).to eq(p1.id)
@@ -65,7 +69,7 @@ describe 'ORM' do
           p1 = RPS.orm.add_player('Gideon', '1234')
           p2 = RPS.orm.add_player('Jon', '321')
           game = RPS.orm.add_game(p1.id, p2.id)
-          RPS.orm.mark_winner(game, p1.id)
+          RPS.orm.mark_winner(game.id, p1.id)
           RPS.orm.mark_round( p1.id, p2.id )
           p1 = RPS.orm.select_player( p1.username )
           game = RPS.orm.select_game( game.id )
@@ -107,23 +111,23 @@ describe 'ORM' do
       end
     end
 
-    # describe "#count_rounds" do
-    #   context "the game is not over" do
-    #     it "" do
-    #       p1 = RPS.orm.add_player('Gideon', 'gewulf', '1234')
-    #       p2 = RPS.orm.add_player('Jon', 'meowmix', '321')
-    #       game = RPS.orm.add_game(p1.id, p2.id, -1)
-    #     end
-    #   end
+    describe "#count_rounds" do
+      context "the game is not over" do
+        it "" do
+          p1 = RPS.orm.add_player('Gideon', 'gewulf', '1234')
+          p2 = RPS.orm.add_player('Jon', 'meowmix', '321')
+          game = RPS.orm.add_game(p1.id, p2.id, -1)
+        end
+      end
 
-    #   context "the game is over" do
-    #     it "" do
-    #       p1 = RPS.orm.add_player('Gideon', 'gewulf', '1234')
-    #       p2 = RPS.orm.add_player('Jon', 'meowmix', '321')
-    #       game = RPS.orm.add_game(p1.id, p2.id, -1)
-    #     end
-    #   end
-    # end
+      context "the game is over" do
+        it "" do
+          p1 = RPS.orm.add_player('Gideon', 'gewulf', '1234')
+          p2 = RPS.orm.add_player('Jon', 'meowmix', '321')
+          game = RPS.orm.add_game(p1.id, p2.id, -1)
+        end
+      end
+    end
   end
 
   describe 'rounds table' do
@@ -171,6 +175,48 @@ describe 'ORM' do
         RPS.orm.add_move(round.id, p2.id, 's')
 
         expect(RPS.orm.select_round(round.id).winner).to eq(1)
+      end
+    end
+
+    describe '#check_if_gameover' do
+      # changes winner of game, wins by player, and winner of round
+      it 'says player1 wins' do
+        RPS.orm.add_player('test', 'tester')
+        RPS.orm.add_player('test1', 'tester1')
+        p1 = RPS.orm.add_player('Gideon', '1234')
+        p2 = RPS.orm.add_player('Jon', '321')
+        g = RPS.orm.add_game(p1.id, p2.id)
+        round = RPS.orm.initialize_round(g.id, 'r')
+        RPS.orm.add_move(round.id, p2.id, 's')
+        round = RPS.orm.initialize_round(g.id, 'r')
+        RPS.orm.add_move(round.id, p2.id, 's')
+        round = RPS.orm.initialize_round(g.id, 'r')
+        RPS.orm.add_move(round.id, p2.id, 's')
+
+        RPS.orm.check_if_gameover(g.id) # method start chain of events
+        expect(RPS.orm.select_game(g.id).winner).to eq(p1.id)
+        expect(RPS.orm.select_round(round.id).winner).to eq(1) # 1 for player2
+        expect(RPS.orm.select_player_by_id(p1.id).win_count).to eq(p1.win_count+1)
+        expect(RPS.orm.select_player_by_id(p2.id).win_count).to eq(p2.win_count)
+      end
+      it 'says player2 wins' do
+        RPS.orm.add_player('test', 'tester')
+        RPS.orm.add_player('test1', 'tester1')
+        p1 = RPS.orm.add_player('Gideon', '1234')
+        p2 = RPS.orm.add_player('Jon', '321')
+        g = RPS.orm.add_game(p1.id, p2.id)
+        round = RPS.orm.initialize_round(g.id, 's')
+        RPS.orm.add_move(round.id, p2.id, 'r')
+        round = RPS.orm.initialize_round(g.id, 's')
+        RPS.orm.add_move(round.id, p2.id, 'r')
+        round = RPS.orm.initialize_round(g.id, 's')
+        RPS.orm.add_move(round.id, p2.id, 'r')
+
+        RPS.orm.check_if_gameover(g.id) # method start chain of events
+        expect(RPS.orm.select_game(g.id).winner).to eq(p2.id)
+        expect(RPS.orm.select_round(round.id).winner).to eq(2) # 1 for player2
+        expect(RPS.orm.select_player_by_id(p2.id).win_count).to eq(p2.win_count+1)
+        expect(RPS.orm.select_player_by_id(p1.id).win_count).to eq(p1.win_count)
       end
     end
   end
